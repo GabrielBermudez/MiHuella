55e0903a1ba5200399c916c7eb6f274d1925ef50
Gabriel M. Bermudez <gabrielbermudez0@gmail.com>
2022-03-24 23:51:26 -0300

    Subo desafio tres

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { CardActionArea } from '@mui/material';
+import Producto_1 from '../images/producto_1.png';
+import Button from '@mui/material/Button';
+
+export default function ItemCount({stocks, initial}) {
+
+    const [itemCount, setItemCount] = useState(initial);
+    const [stock, setStock] = useState(stocks)
+
+    const addItem = () => {
+        if(itemCount < stock)
+            setItemCount(itemCount + 1);
+    }
+
+    const decresedItem = () => {
+        if(itemCount > 0)
+            setItemCount(itemCount -1);
+    }
+
+  return (
+    
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={Producto_1}
+          alt="Producto en venta"
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Stock: {stock}
+            </Typography>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                    <Button variant="outlined" onClick={decresedItem} style={{textDecoration: 'none'}}>-</Button>
+                    </Grid>
+                    <Grid item xs={4}>
+                    <h2>{itemCount}</h2>
+                    </Grid>
+                    <Grid item xs={4}>
+                    <Button variant="outlined" onClick={addItem} style={{textDecoration: 'none'}}>+</Button>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Button variant="contained" style={{textDecoration: 'none'}}>Agregar al Carrito</Button>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import ItemCount from './ItemCount';
 
-const ItemListContainer = () => {
+const ItemListContainer = ({title}) => {
     return (
         <>
             <Container maxWidth="xl">
-                <div style={{textAlign: 'center', textDecoration: 'underline' , boxShadow: '1px 5px 15px gray', height: '50vh'}}>
-                   <h1>Catalogo de Productos</h1>
-                </div>
-
+                <div style={{textAlign: 'center', textDecoration: 'underline' , boxShadow: '1px 5px 15px gray', height: '100vh'}}>
+                   <h1>{title}</h1>
+                   <ItemCount stocks={5} initial={1}/>
+                </div>  
             </Container>
         </>
     );
