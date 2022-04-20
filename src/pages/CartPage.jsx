e4d37e12389088147d983b98523893c34e8e0b15
Gabriel M. Bermudez <gabriel.bermudez@possumus.tech>
2022-04-20 05:12:56 -0300

    Subo desafio 12, se agrega la posibilidad de generar una orden de compra, estos datos quedan guardados en firebase

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { styled } from "@mui/material/styles";
 import CardMedia from "@mui/material/CardMedia";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -12,6 +12,9 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import ModalCustom from "../components/ModalCustom";
+import db from '../firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -43,6 +46,54 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 export default function CartPage() {
   const { cartProducts, removeProductToCart, calculatePriceTotal } = useContext(CartContext);
+  const [showModal, setShowModal] = useState(false);
+  const [successOrder, setSuccessOrder] = useState(false);
+  const [orderID, setOrderID] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: ''
+  });
+  const [order, setOrder] = useState(
+    {
+      buyer: formData,
+      items: cartProducts.map((cartProduct) => {
+        return {
+          id: cartProduct.id,
+          title: cartProduct.title,
+          price: cartProduct.price,
+          quantity: cartProduct.quantity
+        };
+      }),
+      total: calculatePriceTotal()
+    }
+  );
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    let prevOrder = {...order, buyer: formData};
+    e.preventDefault();
+    setOrder({
+      ...order,
+      buyer: formData
+    });
+
+    pushOrder(prevOrder)
+  }
+
+  const pushOrder = async (prevOrder) => {
+    const orderFirebase = collection(db, 'orders');
+    const orderDoc = await addDoc(orderFirebase, prevOrder)
+    setSuccessOrder(true);
+    setOrderID(orderDoc.id);
+  }
 
   return (
     <>
@@ -105,10 +156,10 @@ export default function CartPage() {
           <b>TOTAL: ${calculatePriceTotal()}</b>
         </div>
       </div>
-      <div style={{width: '100%', textAlign: 'center', marginTop: 30}}>
-        <Button variant="contained" style={{textDecoration: 'none'}}>Terminar Compra</Button>
-     </div>
-      {cartProducts.length == 0 && 
+      {cartProducts.length !== 0 && <div style={{width: '100%', textAlign: 'center', marginTop: 30}}>
+        <Button variant="contained" style={{textDecoration: 'none'}} onClick={() => setShowModal(true)}>Terminar Compra</Button>
+      </div>}
+      {cartProducts.length === 0 && 
               <div style={{width: '100%', textAlign: 'center', color:'gray'}}>
                   <h1>No hay productos en el carrito de compras</h1>
                   <Link to="/">
@@ -116,6 +167,22 @@ export default function CartPage() {
                   </Link>
               </div>
               }
+      { showModal && <ModalCustom handleClose={() => setShowModal(false)} open={showModal}>
+        {(successOrder) ? (
+          <div style={{width: '100%', textAlign: 'center', padding: '10px 0px'}}>
+            <h2 style={{color:"green", textAlign:'center'}}>Orden Generada con el codigo: {orderID}</h2>
+            <Link to="/"><Button variant="contained" style={{textDecoration: 'none'}}>Aceptar</Button></Link>
+          </div>
+        ): (
+          <div style={{padding:20, textAlign:'center'}}>
+        <h2 style={{textDecoration:'underline'}}>Formulario de Compra</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Nombre" onChange={handleChange} value={formData.name} style={{marginBottom:5}} /><br />
+          <input type="number" name="phone" placeholder="Telefono" onChange={handleChange} value={formData.phone} style={{marginBottom:5}} /><br />
+          <input type="mail" name="email" placeholder="Email" onChange={handleChange} value={formData.email} style={{marginBottom:5}} /><br />
+          <Button variant="contained" type="submit">Enviar</Button>
+        </form> </div>)}
+      </ModalCustom> }
     </>
   );
 }
